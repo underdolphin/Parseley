d4f4b3e80f259298ccc350803cd0fa2ab9006f4c
underdolphin <underdolphin@users.noreply.github.com>
2016-12-06 23:11:57 +0900

    Improve task runner

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,42 @@
-/// <reference path="typings/index.d.ts" />
 'use strict'
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const gulpts = require('gulp-typescript');
-const prettydiff = require('gulp-prettydiff');
+const uglify = require('gulp-uglify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 
 const tsProject = gulpts.createProject('tsconfig.json', {
     typescript: require('typescript'),
-    module: "commonjs"
+    module: "commonjs",
+    sortOutput: true
 });
 
 gulp.task('tsCompile', () => {
     gulp.src('src/**/*.ts')
         .pipe(plumber())
-        .pipe(gulpts(tsProject))
+        .pipe(tsProject())
         .js
-        .pipe(prettydiff({
-            lang: 'js',
-            mode: 'minify'
-        }))
         .pipe(gulp.dest('build/'));
 });
 
-gulp.task('start', ['tsCompile'], () => {
-    gulp.watch('src/**/*.ts', ['tsCompile']);
+gulp.task('browserify', () => {
+    browserify({
+            entries: ['./build/index.js']
+        })
+        .bundle()
+        .pipe(source('lib.min.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('default', ['start'], () => {
+gulp.task('watch', () => {
+    gulp.watch('src/**/*.ts', ['tsCompile']);
+    gulp.watch('build/**/*.js', ['browserify']);
+});
 
+gulp.task('default', ['tsCompile', 'browserify'], () => {
+    gulp.start('watch')
 });
