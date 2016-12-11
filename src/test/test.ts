3f47cf5a6f2bff05de73d812c025f4a407e87b89
underdolphin <underdolphin@users.noreply.github.com>
2016-12-11 20:44:33 +0900

    Add parse method

---
+++ src/test/test.ts
@@ -0,0 +1,22 @@
+"use strinct"
+import { Parseley } from '../parseley';
+import * as chai from 'chai';
+const assert = chai.assert;
+
+describe('parse function', () => {
+    let parseley: Parseley = null;
+    it('when inner variant setup, add a target string and position.', () => {
+        parseley = new Parseley().parse('parse string', 0);
+        assert.strictEqual(parseley.success, true);
+        assert.strictEqual(parseley.result[0], '');
+        assert.strictEqual(parseley.newPosition, 0);
+        assert.strictEqual(parseley.targetString, 'parse string');
+    });
+    it('Inner variant no setup.', () => {
+        parseley = new Parseley().parse('', 0);
+        assert.strictEqual(parseley.success, false);
+        assert.strictEqual(parseley.result[0], '');
+        assert.strictEqual(parseley.newPosition, 0);
+        assert.strictEqual(parseley.targetString, '');
+    });
+});
